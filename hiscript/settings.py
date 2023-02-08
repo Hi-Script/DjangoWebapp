9329a29e1659f3466e846a2af3ba3ecaf3efdd32
HiScript <tundeadeoye9@gmail.com>
2023-02-08 09:04:28 +0100

    bol

--- hiscript/settings.py
+++ hiscript/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'hiscript.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config (
-        default='sqlite:///' + os.path.join('db.sqlite3')
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
     )
 }
 
