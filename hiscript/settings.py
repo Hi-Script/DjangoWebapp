dcef9a04f69d032208ed49259ca60588f64678ef
Taoheed <106952791+Hi-Script@users.noreply.github.com>
2023-03-23 16:01:07 +0100

    Update settings.py

--- hiscript/settings.py
+++ hiscript/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '*']
 #CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
