1e994e12f3b8c5c5872347c89851daf576a8667e
Taoheed <106952791+Hi-Script@users.noreply.github.com>
2023-03-23 16:41:30 +0100

    Update settings.py

--- hiscript/settings.py
+++ hiscript/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [  'localhost', '127.0.0.1', 'hiscript-5faj.onrender.com', 'www.hiscript-5faj.onrender.com' ]
+ALLOWED_HOSTS = [  '*' ]
 #CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
 #ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['hiscript-5faj.onrender.com']
